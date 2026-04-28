e4479ae1054c3f00f5890bb3827bedbadc75ede8
rawan ayman <ronyayman68@gmail.com>
2026-04-28 22:15:57 +0300

    Updates

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,14 +1,14 @@
-import './skill.css'; 
-import React from 'react';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import "./skill.css";
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
     desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
     tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
-    mobile: { breakpoint: { max: 464, min: 0 }, items: 1 } 
+    mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
   };
 
   return (
@@ -18,8 +18,15 @@ export const Skills = () => {
           <div className="col-12">
             <div className="skill-bx">
               <h2>Skills</h2>
-              <p>Specializing in full-stack development and creative web solutions.</p>
-              <Carousel responsive={responsive} infinite={true} className="skill-slider">
+              <p>
+                Specializing in full-stack development and creative web
+                solutions.
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider"
+              >
                 <div className="item">
                   <div className="circle-bg">95%</div>
                   <h5>Web Development</h5>
